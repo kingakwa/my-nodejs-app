ff1d4b53a5cfde853f4d94b91c7081609adc8086
Akwa B. NJIE <akwaking@gmail.com>
2025-07-31 18:38:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Node.js CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from Node.js CI/CD! CI/CD for Node.js App using GitHub Actions and AWS EC2'));
 app.listen(3000, () => console.log('App listening on port 3000'));
 
