3dcb073f985708ac36da89e3b3c13945294e885f
Akwa <akwaking@gmail.com>
2025-07-31 15:30:51 +0100

    Initial CI/CD Node.js app setup

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Node.js CI/CD!'));
+app.listen(3000, () => console.log('App listening on port 3000'));
+
